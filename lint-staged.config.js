b434f96ba653c9f263d47153bcde8da8bab9de00
James Brennan <james@jamesbrennan.ca>
2020-04-16 18:02:01 +0000

    CO-1757 Duplicate lint-staged matches do not work

--- lint-staged.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "*.{ts,tsx}": [
-    "tslint --config tslint.json --project tsconfig.json --fix",
-    "prettier --write"
-  ],
-  // Use this configuration format to not pass filename arguments to `npm run build`
-  "**/*.ts": () => ['npm run build', 'git add dist/index.js'],
-  "package(-lock)?.json": () => ['npm run build', 'git add dist/index.js'],
-};
