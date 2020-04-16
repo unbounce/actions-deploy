f49e6f8819cec14a35ac072cd2df64a3e813ceec
James Brennan <james@jamesbrennan.ca>
2020-04-16 17:06:32 +0000

    CO-1757 Rebuild when package.json changes

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "tslint --config tslint.json --project tsconfig.json --fix",
     "prettier --write"
   ],
-  // Use this configuration format to not pass filename arguements
-  "**/*.ts": () => ['npm run build', 'git add dist/index.js']
+  // Use this configuration format to not pass filename arguments to `npm run build`
+  "**/*.ts": () => ['npm run build', 'git add dist/index.js'],
+  "package(-lock)?.json": () => ['npm run build', 'git add dist/index.js'],
 };
